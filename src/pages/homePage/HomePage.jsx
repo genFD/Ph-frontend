4a908cd030466f36e89fcf1a727e1ad0b926ec64
Hermann <hermann.kan@icloud.com>
2022-01-09 21:43:20 -0500

    added link to home and login

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,50 +1,70 @@
 import React, { useState } from 'react'
-import {FaShoppingCart, FaUserAstronaut} from 'react-icons/fa'
+import {FaShoppingCart, FaUserAstronaut, FaHeadphones} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import './homePage.css'
 
 const HomePage = () => {
  return (
   <div className='container'>
    <Navbar/>
-    <h1>Home</h1>
+    <ProductsList/>
   </div>
  )
 }
 
-const Navbar = ()=>{
- const [openNav, setOpenNav] = useState(false)
- return (
-  <nav  id="nav">
-      <span>Logo</span>
-      <ul className={openNav ? 'active': null}>
-       
-         <button className="icon" id="toggle" onClick={()=> setOpenNav(!openNav)}>
-        <div className="line line1"></div>
-        <div className="line line2"></div>
-      </button>
-      <div>
-        <li><a href="">
-         <FaShoppingCart size={18} style={{
-          color:'#64ffda'
-         }}/>
-         </a></li>
-        <li><a href="">
-         <FaUserAstronaut
-         size={18}
-         style={{
-          color:'#64ffda'
-         }}
-         />
-         </a></li>
-        {/* <li><a href="">light</a></li> */}
-       </div>
+const Navbar = () => {
+  const [openNav, setOpenNav] = useState(false);
+  return (
+    <nav id='nav'>
+      <span>
+        <Link to='/'>
+          <FaHeadphones
+            size={30}
+            style={{
+              color: '#64ffda',
+            }}
+          />
+        </Link>
+      </span>
+      <ul className={openNav ? 'active' : null}>
+        <button
+          className='icon'
+          id='toggle'
+          onClick={() => setOpenNav(!openNav)}>
+          <div className='line line1'></div>
+          <div className='line line2'></div>
+        </button>
+        <div>
+          <li>
+            <Link to='/'>
+              <FaShoppingCart
+                size={18}
+                style={{
+                  color: '#64ffda',
+                }}
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to='/login'>
+              <FaUserAstronaut
+                size={18}
+                style={{
+                  color: '#64ffda',
+                }}
+              />
+            </Link>
+          </li>
+          {/* <li><a href="">light</a></li> */}
+        </div>
       </ul>
     </nav>
-    
- )  
- 
- 
-}
+  );
+};
+
+const ProductsList = () => {
+  return <div>ProductsList</div>;
+};
 
 
 
